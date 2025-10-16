3de5f4314481403d7e5460a0f2eb2180f0476e2f
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-10-15 20:54:43 -0600

    Update Next.js version and enhance configuration settings

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,15 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   experimental: {
     optimizePackageImports: ['lucide-react', 'framer-motion', 'react-icons'],
   },
+  reactStrictMode: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
